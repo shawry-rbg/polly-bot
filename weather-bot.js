f571871a5a564787116b76444df4a179144d9efc
githubb <elvischemoiywo@gmail.com>
2026-05-07 17:27:13 +0300

    Update weather-bot.js

--- weather-bot.js
+++ weather-bot.js
@@ -1,4 +1,9 @@
 const axios=require('axios');require('dotenv').config();
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('OK'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Health check running on port ${port}`));
 const KEY=process.env.GROQ_API_KEY;
 const MODEL='llama-3.1-8b-instant';
 
